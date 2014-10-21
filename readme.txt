839e2f5f1b314eb1209365c9bedb77ab251a8c62
SwiftCloud <SwiftCloud@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-21 20:28:27 +0000

    .1 Alpha
    git-svn-id: https://plugins.svn.wordpress.org/swiftcloud/trunk@1011734 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,69 @@
+=== Plugin Name ===
+Contributors: SwiftCloud
+Tags: forms, web forms, polls, lead capture, landing page, 
+Requires at least: 3.0
+Tested up to: 4.0
+Stable tag: 4.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Embeds SwiftForm.com (free HTML Forms generator) webforms via shortcode or widget tied to SwiftCloud apps. Free Signup at http://SwiftCloud.me
+
+== Description ==
+SwiftCloud simply connects your Wordpress site/blog to http://SwiftCloud.me functions.
+
+*This plugin requires a (free or paid) account on http://SwiftCloud.me to do anything useful.*
+
+SwiftCloud is a business productivity suite in alpha, mostly of value to web designers and developers.
+
+http://SwiftForm.com is currently 100% free and will always have free options. If you just want to make a 
+nice looking form that emails you each time it is filled out, try it out.
+
+Features of SwiftForm:
+1. Fields like "Smart Zip" will automatically convert your zip code or postal code to city, state, zip, county, time zone
+1. Time zone, language settings and other meta will be stored automatically
+1. The complete URL of the page on which the form-data is sent from will be captured, including Google Analytics and other tracking data which will help with marketing, specifically, knowing how this person found you.
+1. More features coming soon.
+
+*This plugin and related code are evolving rapidly so please expect weekly updates through the end of 2014.*
+
+Currently this plugin does just one thing: Embed SwiftForm.com forms (a free drag and drop forms editor) into any Wordpress
+blog / website, using either widgets or shortcodes (using [swiftform formid=XXXXXX] where XXXXX = your form number, as provided
+within the SwiftForm.com app.
+
+These forms can then be automatically connected to...
+* Autoresponder Sequences (in http://SwiftMarketing.com) 
+* SwiftCRM Hosted Client Relationship Manager (http://SwiftCRM.com)
+* SwiftBooks accounting (http://SwiftBooks.com)
+* SwiftTasks task/project management system (locked in Private Alpha as of Oct 2014)
+
+with additional use cases planned to follow. We welcome your feedback and requests.
+
+== Installation ==
+You probably know the drill by now, but if not, here's a step by step suggestion.
+
+1. Upload the `SwiftCloud_WP` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. To install a webform, login at http://SwiftForm.com (free signup) and click 'new form',
+drag and drop fields to create a form, click save, and then remember the number it gives you.
+1. Either drop a shortcode 
+
+== Frequently Asked Questions ==
+
+1. How do I use it?
+
+See above. You need a SwiftCloud account (free for most applications) (sign up at http://SwiftCloud.me to use any 
+SwiftCloud apps). To drop into a page-body, use shortcode [swiftform formid=XXXXXX] where XXXXX is your form number,
+or drag and drop a widget on the Appearance >> Widgets section.
+
+== Screenshots ==
+
+Coming soon
+
+== Changelog ==
+
+= 0.2 =
+* Alpha release.
+
+
+`<?php code(); // goes in backticks ?>`
---
+++ swiftformwidget.php
@@ -0,0 +1,146 @@
+<?php
+/*
+		Plugin Name: SwiftCloud
+		Plugin URL: http://kb.SwiftCloud.me/wordpress-plugin
+		Description: Allows you to include Swift Forms as widget in Sidebars
+		Version: 1.0
+		Author: Roger Vaughn, Sajid Javed
+		Author URI: http://SwiftCloud.me/
+	*/
+class Swiftform_Widget extends WP_Widget
+{
+	var $ErrorMessage = 'Form ID is required to display form!';
+	 /**
+	  * Declares the Swiftform_Widget class.
+	  */
+    function Swiftform_Widget(){
+    $widget_ops = array('classname' => 'widget_swiftform', 'description' => __( "SwiftForm Widget Setup") );
+    $control_ops = array('width' => 300, 'height' => 300);
+    $this->WP_Widget('swiftform', __('Swiftform'), $widget_ops, $control_ops);
+	add_shortcode('swiftform', array($this, 'dispaly_swiftform') );
+	
+    }
+	
+	function dispaly_swiftform($atts, $content = ''){
+		
+		$atts = shortcode_atts(
+			array(
+				'formid' => '',
+ 			), $atts );
+		
+		extract($atts);		
+		
+		if( empty($formid) ) return $this->ErrorMessage;
+		
+  		$formID = $atts['formid'];
+		
+		$readFormUrl = 'http://swiftform.com/f/'.$formID;
+		//exit($readFormUrl);
+		
+		// make sure curl is installed
+		if (function_exists('curl_init')) {
+		   // initialize a new curl resource
+		   $ch = curl_init();
+		
+		   // set the url to fetch
+		   curl_setopt($ch, CURLOPT_URL, $readFormUrl);
+		
+		   // don't give me the headers just the content
+		   curl_setopt($ch, CURLOPT_HEADER, 0);
+		
+		   // return the value instead of printing the response to browser
+		   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		   
+		   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		
+		   // use a user agent to mimic a browser
+		   curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.5) Gecko/20041107 Firefox/1.0');
+		
+		   $text = curl_exec($ch);
+		    
+		   // remember to always close the session and free all resources
+		   curl_close($ch);
+		} else {
+		   // curl library is not installed so we better use something else
+		}
+		return $text;
+	}
+	
+  	/**
+    * Displays the Widget
+    *
+    */
+    function widget($args, $instance){
+			 
+      extract($args);
+      $title = apply_filters('widget_title', empty($instance['title']) ? '&nbsp;' : $instance['title']);
+      $formID = empty($instance['formID']) ? '' : $instance['formID'];
+     
+      # Before the widget
+      echo $before_widget;
+
+      # The title
+      if ( $title )
+      echo $before_title . $title . $after_title;
+	
+      # Make the widget show the form
+	  if($formID!="") {
+		  
+		  $formData = array('formid' => $formID);
+		  
+		  $text = $this->dispaly_swiftform($formData);
+ 		
+		 global $iSubscriberId;
+		 if($iSubscriberId>0)
+		 $text = preg_replace('/name="iSubscriberId"  value="(\d*)"/','name="iSubscriberId"  value="'.$iSubscriberId.'"',$text);
+	
+		 echo $text;
+		 
+	  }else{
+		  echo $this->ErrorMessage;
+	  }
+	  
+
+      # After the widget
+      echo $after_widget;
+  }
+
+  /**
+    * Saves the widgets settings.
+    */
+    function update($new_instance, $old_instance){
+      $instance = $old_instance;
+      $instance['title'] = strip_tags(stripslashes($new_instance['title']));
+      $instance['formID'] = strip_tags(stripslashes($new_instance['formID']));
+
+    return $instance;
+  }
+
+  /**
+    * Creates the edit form for the widget.
+    */
+    function form($instance){
+      //Defaults
+      $instance = wp_parse_args( (array) $instance, array('title'=>'', 'formID'=>'') );
+
+      $title = htmlspecialchars($instance['title']);
+      $formID = htmlspecialchars($instance['formID']);
+      
+      # Output the options
+      echo '<p style="text-align:right;"><label for="' . $this->get_field_name('title') . '">' . __('Title:') . ' <input style="width: 250px;" id="' . $this->get_field_id('title') . '" name="' . $this->get_field_name('title') . '" type="text" value="' . $title . '" /></label></p>';
+      # Text line 1
+      echo '<p style="text-align:right;"><label for="' . $this->get_field_name('formID') . '">' . __('Form ID:') . ' <input style="width: 200px;" id="' . $this->get_field_id('formID') . '" name="' . $this->get_field_name('formID') . '" type="text" value="' . $formID . '" /></label></p>';
+  }
+
+}// END class
+
+/**
+  * Register Swift Form Widget
+  *
+  * Calls 'widgets_init' action after the Hello World widget has been registered.
+  */
+  function Swiftform_Widget_Init() {
+  register_widget('Swiftform_Widget');
+  }
+  add_action('widgets_init', 'Swiftform_Widget_Init');
+?>
